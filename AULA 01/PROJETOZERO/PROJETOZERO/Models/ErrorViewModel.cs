06fa8fea979c594caccdb09f07cf5845cec2a8fd
frtavares <43138845+frtavares@users.noreply.github.com>
2019-09-14 17:53:01 -0300

    Projeto Zero

---
+++ AULA 01/PROJETOZERO/PROJETOZERO/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PROJETOZERO.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
